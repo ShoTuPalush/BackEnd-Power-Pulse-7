a9e985c6e57370ca2a9cd8d61eaea7c1119ebfcb
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2024-03-04 20:16:45 +0200

    clear midl

--- routes/trainingRoutes.js
+++ routes/trainingRoutes.js
@@ -4,7 +4,7 @@ const authMiddlewares = require("../middlewares/authMiddlewares");
 
 const trainingRoutes = express.Router();
 
-trainingRoutes.get("/", authMiddlewares, ctrl.getCategory);
-trainingRoutes.get("/all", authMiddlewares, ctrl.getAll);
+trainingRoutes.get("/", ctrl.getCategory);
+trainingRoutes.get("/all", ctrl.getAll);
 
 module.exports = trainingRoutes;
