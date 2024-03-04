097ed4c4ea6dd1738158dab496c09f33235f0b17
rmlyaaa <126497199+rmlyaaa@users.noreply.github.com>
2024-03-04 15:05:24 +0100

    upd router

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,6 +5,6 @@ const authMiddlewares = require("../middlewares/authMiddlewares");
 const productsRoutes = express.Router();
 
 productsRoutes.get("/", authMiddlewares, ctrl.getCategory);
-productsRoutes.get("/all", ctrl.getAll);
+productsRoutes.get("/all", authMiddlewares, ctrl.getAll);
 
 module.exports = productsRoutes;
