487530ce99634a9aa69acf2953c3cb4b2dbf33e3
rmlyaaa <126497199+rmlyaaa@users.noreply.github.com>
2024-03-04 20:12:19 +0100

    upd pagination limit

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -8,7 +8,7 @@ const getCategory = async (req, res) => {
 };
 
 const getAll = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
+  const { page, limit } = req.query;
   const skip = (page - 1) * limit;
   const { category, title, blood, recomended } = req.query;
   let filters = {};
