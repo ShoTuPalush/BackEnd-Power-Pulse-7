0aa7a0522ae4f3f39f40e18fd85e55f73969daf4
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2024-03-14 01:28:19 +0200

    Update generateVerifyMessage.js

--- helpers/generateVerifyMessage.js
+++ helpers/generateVerifyMessage.js
@@ -2,10 +2,10 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const { FRONTEND_URL } = process.env;
-const url = FRONTEND_URL || 'http://localhost:3000';
+const url = FRONTEND_URL || 'http://localhost:5173';
 
-const generateVerifyMessage = verificationToken =>
-  `<html>
+{
+  /* <html>
       <head>
         <style>
           body {
@@ -40,6 +40,336 @@ const generateVerifyMessage = verificationToken =>
           <a target="_blank" href="${url}/?verificationToken=${verificationToken}">Click for verify email</a>
         </div>
       </body>
-    </html>`;
+    </html> */
+}
+
+const generateVerifyMessage = verificationToken =>
+  `<head>
+  <meta charset="utf-8">
+  <meta name="x-apple-disable-message-reformatting">
+  <style amp4email-boilerplate>
+    body {
+      visibility: hidden
+    }
+  </style>
+
+  <script async src="https://cdn.ampproject.org/v0.js"></script>
+
+
+  <style amp-custom>
+    .u-row {
+      display: flex;
+      flex-wrap: nowrap;
+      margin-left: 0;
+      margin-right: 0;
+    }
+    
+    .u-row .u-col {
+      position: relative;
+      width: 100%;
+      padding-right: 0;
+      padding-left: 0;
+    }
+    
+    .u-row .u-col.u-col-100 {
+      flex: 0 0 100%;
+      max-width: 100%;
+    }
+    
+    @media (max-width: 767px) {
+      .u-row:not(.no-stack) {
+        flex-wrap: wrap;
+      }
+      .u-row:not(.no-stack) .u-col {
+        flex: 0 0 100%;
+        max-width: 100%;
+      }
+    }
+    
+    body {
+      margin: 0;
+      padding: 0;
+    }
+    
+    table,
+    tr,
+    td {
+      vertical-align: top;
+      border-collapse: collapse;
+    }
+    
+    p {
+      margin: 0;
+    }
+    
+    .ie-container table,
+    .mso-container table {
+      table-layout: fixed;
+    }
+    
+    * {
+      line-height: inherit;
+    }
+    
+    table,
+    td {
+      color: #000000;
+    }
+    
+    #u_body a {
+      color: #0000ee;
+      text-decoration: underline;
+    }
+    
+    @media (max-width: 480px) {
+      #u_content_image_1 .v-container-padding-padding {
+        padding: 0px;
+      }
+    }
+  </style>
+
+
+</head>
+
+<body class="clean-body u_body" style="margin: 0;padding: 0;background-color: #f9f9f9;color: #000000">
+  <!--[if IE]><div class="ie-container"><![endif]-->
+  <!--[if mso]><div class="mso-container"><![endif]-->
+  <table id="u_body" style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #f9f9f9;width:100%" cellpadding="0" cellspacing="0">
+    <tbody>
+      <tr style="vertical-align: top">
+        <td style="word-break: break-word;border-collapse: collapse;vertical-align: top">
+          <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color: #f9f9f9;"><![endif]-->
+
+          <div style="padding: 0px;">
+            <div style="max-width: 600px;margin: 0 auto;">
+              <div class="u-row">
+
+                <div class="u-col u-col-100" style="display:flex;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+                  <div style="width: 100%;padding:0px;">
+
+                    <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <div style="font-size: 14px; color: #afb0c7; line-height: 170%; text-align: center; word-wrap: break-word;">
+                              <p style="font-size: 14px; line-height: 170%;"><span style="font-size: 14px; line-height: 23.8px;">View Email in Browser</span></p>
+                            </div>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div style="padding: 0px;">
+            <div style="max-width: 600px;margin: 0 auto;background-color: #ffffff;">
+              <div class="u-row">
+
+                <div class="u-col u-col-100" style="display:flex;background-color:#000000;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+                  <div style="width: 100%;padding:0px;">
+
+                    <table id="u_content_image_1" style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:0px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                              <tr>
+                                <td style="padding-right: 0px;padding-left: 0px;" align="center">
+                                  <a href="https://sergeycrew.github.io/project-Power-7/" target="_blank">
+                                    <amp-img alt="Image" src="https://res.cloudinary.com/dxqzi4x9j/image/upload/v1710372374/p9al1gdnd9i1jfgffef4.png" width="932" height="188" layout="intrinsic" style="width: 100%;max-width: 932px;">
+                                  </a>
+                                  </amp-img>
+                                </td>
+                              </tr>
+                            </table>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div style="padding: 0px;">
+            <div style="max-width: 600px;margin: 0 auto;background-color: #003399;">
+              <div class="u-row">
+
+                <div class="u-col u-col-100" style="display:flex;background-color:#000000;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+                  <div style="width: 100%;padding:0px;">
+
+                    <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:40px 10px 10px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                              <tr>
+                                <td style="padding-right: 0px;padding-left: 0px;" align="center">
+
+                                  <amp-img alt="Image" src="https://cdn.templates.unlayer.com/assets/1597218650916-xxxxc.png" width="335" height="93" layout="intrinsic" style="width: 26%;max-width: 26%;">
+
+                                  </amp-img>
+                                </td>
+                              </tr>
+                            </table>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                    <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <div style="font-size: 14px; color: #e5eaf5; line-height: 140%; text-align: center; word-wrap: break-word;">
+                              <p style="font-size: 14px; line-height: 140%;"><strong>T H A N K S&nbsp; &nbsp;F O R&nbsp; &nbsp;S I G N I N G&nbsp; &nbsp;U P !</strong></p>
+                            </div>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                    <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:0px 10px 31px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <div style="font-size: 14px; color: #e5eaf5; line-height: 140%; text-align: center; word-wrap: break-word;">
+                              <p style="font-size: 14px; line-height: 140%;"><span style="font-size: 28px; line-height: 39.2px;"><strong><span style="line-height: 39.2px; font-size: 28px;">Verify Your E-mail Address </span></strong>
+                                </span>
+                              </p>
+                            </div>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div style="padding: 0px;">
+            <div style="max-width: 600px;margin: 0 auto;background-color: #ffffff;">
+              <div class="u-row">
+
+                <div class="u-col u-col-100" style="display:flex;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+                  <div style="width: 100%;padding:0px;">
+
+                    <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:33px 55px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <div style="font-size: 14px; line-height: 160%; text-align: center; word-wrap: break-word;">
+                              <p style="font-size: 14px; line-height: 160%;"><span style="font-size: 22px; line-height: 35.2px;">Hi, </span></p>
+                              <p style="font-size: 14px; line-height: 160%;"><span style="font-size: 18px; line-height: 28.8px;">You're almost ready to get started. Please click on the button below to verify your email address and start t<span style="line-height: 22.4px;">ransforming your body shape </span>with
+                                us today! </span>
+                              </p>
+                            </div>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                    <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <!--[if mso]><style>.v-button {background: transparent;}</style><![endif]-->
+                            <div align="center">
+                              <!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" style="height:46px; v-text-anchor:middle; width:235px;" arcsize="8.5%"  stroke="f" fillcolor="#ff6600"><w:anchorlock/><center style="color:#FFFFFF;"><![endif]-->
+                              <a target="_blank" href="${url}/profile/?verificationToken=${verificationToken}" class="v-button" style="box-sizing: border-box;display: inline-block;text-decoration: none;text-align: center;color: #FFFFFF; background-color: #ff6600; border-radius: 4px;  width:auto; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; font-size: 14px;">
+                                <span style="display:block;padding:14px 44px 13px;line-height:120%;"><span style="font-size: 16px; line-height: 19.2px;"><strong><span style="line-height: 19.2px; font-size: 16px;">VERIFY YOUR EMAIL</span></strong>
+                                </span>
+                                </span>
+                              </a>
+                              <!--[if mso]></center></v:roundrect><![endif]-->
+                            </div>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                    <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:33px 55px 60px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <div style="font-size: 14px; line-height: 160%; text-align: center; word-wrap: break-word;">
+                              <p style="line-height: 160%; font-size: 14px;"><span style="font-size: 18px; line-height: 28.8px;">Thanks,</span></p>
+                              <p style="line-height: 160%; font-size: 14px;"><span style="font-size: 18px; line-height: 28.8px;">Power Pulse Team</span></p>
+                            </div>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div style="padding: 0px;">
+            <div style="max-width: 600px;margin: 0 auto;background-color: #003399;">
+              <div class="u-row">
+
+                <div class="u-col u-col-100" style="display:flex;background-color:#000000;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+                  <div style="width: 100%;padding:0px;">
+
+                    <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <div style="font-size: 14px; color: #ffffff; line-height: 180%; text-align: center; word-wrap: break-word;">
+                              <p style="font-size: 14px; line-height: 180%;"><span style="font-size: 16px; line-height: 28.8px;">Copyrights © Company All Rights Reserved</span></p>
+                            </div>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <!--[if mso]></div><![endif]-->
+  <!--[if IE]></div><![endif]-->
+</body>`;
 
 module.exports = generateVerifyMessage;
